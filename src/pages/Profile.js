fdaaa68d5ad45bff54aec4a4ff4f3d39de68de0e
Andre Jaques <andrejaques1994@gmail.com>
2021-05-17 11:10:24 -0300

    incluindo botao no profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function Profile() {
+  const navigate = useNavigate();
   return (
     
+     <>
      <h1>Profile</h1>
+     <button onClick={()=> navigate('/')}>Dashboard</button>
+      <button onClick={()=> navigate('/Teste')}>teste</button>
+     </>
   
   );
 }
